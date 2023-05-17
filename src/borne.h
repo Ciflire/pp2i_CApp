c50525e510accaed5f097c11a22728de1b2d0474
VESSE Leo <leovesse@gmail.com>
2023-05-17 23:20:10 +0200

    Basic structures done [+] struct vehicule implemented [+] struct borne implemented

---
+++ src/borne.h
@@ -0,0 +1,13 @@
+#ifndef BORNE_H
+#define BORNE_H
+
+typedef struct borne{
+    int id;
+    double xpos;
+    double ypos;
+    double power;
+    int qte;
+};
+
+
+#endif
---
+++ src/vehicule.h
@@ -0,0 +1,12 @@
+#ifndef VEHICULE_H
+#define VEHICULE_H
+
+typedef struct vehicules{
+    int id;
+    char* name;
+    int autonomie;
+};
+
+typedef struct vehicules vehicules;
+
+#endif
