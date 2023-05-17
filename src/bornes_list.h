a2dfb34f17c4aafb31e8faa15daeba252c28a281
VESSE Leo <leovesse@gmail.com>
2023-05-18 00:03:54 +0200

    Bornes_list structure implemented [+] Created the structure without the methods

---
+++ src/bornes_list.h
@@ -0,0 +1,14 @@
+#include "borne.h"
+
+#ifndef BORNES_LIST_H
+#define BORNES_LIST_H
+
+typedef struct bornes_list{
+    int size;
+    borne* next;
+};
+
+typedef struct bornes_list bornes_list;
+
+
+#endif
