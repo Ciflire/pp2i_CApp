c15b91d702c187c5545996b0beb9fc78cf84fe12
Killian <killian.thuillier@telecomnancy.eu>
2023-05-20 20:50:11 +0200

    Mem leak bug fixed

--- src/test/vehicule_list_test.c
+++ src/test/vehicule_list_test.c
@@ -2,8 +2,12 @@
 
 int main(void) {
   vehicule_list *vl = create_vehicule_list();
-  vehicule *v = create_vehicule(1, "test", 2, 3);
-  add_vehicule(vl, v);
+  for (int i=0; i<100; i++) {
+    vehicule *v = create_vehicule(i, "test", 2, 3);
+    add_vehicule(vl, v);
+  }
+  //vehicule *v = create_vehicule(1, "test", 2, 3);
+  //add_vehicule(vl, v);
   destroy_vehicule_list(vl);
   return 0;
 }
