9c7e1e0fb30557392b429f8fcacf9c8ba7f366d6
VESSE Leo <leovesse@gmail.com>
2023-05-24 19:01:37 +0200

    run you clever boy and remember me (Clara Oswald)

--- src/test/bornes_list_test.c
+++ src/test/bornes_list_test.c
@@ -6,6 +6,13 @@ int main(void) {
   {
     add_borne(bl, create_borne(i, i, i, i, i));
   }
+
+  for (int i = 1000; i < 2000; i++)
+  {
+    if (is_borne_in_list(bl, i)){
+      printf("%d,%d\n", is_borne_in_list(bl, i),i);
+    }
+  }
   
   printf("%i\n", get_length(bl));
   destroy_bornes_list(bl);
