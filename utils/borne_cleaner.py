ff508eb53f3d4fa6dc8165ca67a8788f5decd251
VESSE Leo <leovesse@gmail.com>
2023-06-02 18:25:29 +0200

    borne_cleaner is now in a main function

--- utils/borne_cleaner.py
+++ utils/borne_cleaner.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-bornes = pd.read_csv('data/borne_firststep.csv', sep=',').drop_duplicates()
 
+def main():
+  # Reads data, deletes duplicates and saves it
+  bornes = pd.read_csv('data/borne_firststep.csv', sep=',').drop_duplicates()
 
-bornes.to_csv('data/borne_cleaned.csv', sep=',', index=False)
+  # Export to csv
+  bornes.to_csv('data/borne_cleaned.csv', sep=',', index=False)
 
-plt.figure(0, figsize=[50, 50])
-plt.scatter(bornes['consolidated_longitude'], bornes['consolidated_latitude'],
+  # Plot
+  plt.figure(0, figsize=[50, 50])
+  plt.scatter(bornes['consolidated_longitude'], bornes['consolidated_latitude'],
             s=bornes['nbre_pdc'].values*10, c=bornes['puissance_nominale'].values, cmap='plasma')
-plt.savefig('data/borne_cleaned.png')
+  plt.savefig('data/borne_cleaned.png')
+  
+if __name__ == '__main__':
+  main()
