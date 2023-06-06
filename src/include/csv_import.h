edb94809792bee7e4b51f9ed622236220a7c39c0
VESSE Leo <leovesse@gmail.com>
2023-06-06 17:26:24 +0200

    structures added to the project [+] line structure added [+] csv_import structure added

---
+++ src/include/csv_import.h
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINES 14760
+
+#ifndef CSV_IMPORT_H
+
+#define CSV_IMPORT_H
+
+struct line {
+  int size;
+  char **args[];
+};
+
+typedef struct line line;
+
+struct csv_import {
+  int size;
+  line **csv[];
+};
+
+typedef struct csv_import csv_import;
+
+#endif
