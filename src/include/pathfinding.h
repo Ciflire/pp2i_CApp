18fe2427fbc6e6c25d2e2075cd192b81419090a6
VESSE Leo <leovesse@gmail.com>
2023-06-08 14:08:06 +0200

    Corrected version pathfinding.c and pathfinding.h

--- src/include/pathfinding.h
+++ src/include/pathfinding.h
@@ -7,7 +7,9 @@ int creationZone(borne *actual, borne *ending, double p, car *usedCar, borne_lis
 
 int pathFinding(car *usedCar, borne *actual, borne *ending, borne_list *path, borne_list *allListsBorne, int maxTimeWaiting);
 
-void findBestInZone(borne_list *Zone, borne *actual, borne *goal, int maxTimeCharging, int maxTimeWaiting, borne *best, double bestTime);
+void findBestInZone(borne_list *Zone, borne *actual, borne *goal, car *usedCar,
+                    int maxTimeCharging, int maxTimeWaiting, borne *best,
+                    double bestTime);
 
 bool isBorneBetterThanCurrentBestBorne(int bestTime, borne *borneInTest, borne *actual, borne *goal, car *car, int maxTimeWaiting, int maxTimeCharging);
 
