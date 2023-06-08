b71ad6563647c8ecc19bdff35ce0decb623ef4db
VESSE Leo <leovesse@gmail.com>
2023-06-08 13:55:39 +0200

    Merge commit

--- src/test/data_importer_test.c
+++ src/test/data_importer_test.c
@@ -12,42 +12,35 @@ int main(void){
     printf("Time to parse the bornes file : ");
     timer_print();
     borne_list *list_borne = borne_list_create();
-
     //import the data to a bornes_list
     borne_data_importer(file, list_borne);
     timer_stop();
     printf("Time to import the bornes data: ");
     timer_print();
-
     csv_import_destroy(file);
-    //borne_list_print(list_borne);
+    /* borne_list_print(list_borne); */
     borne_list_destroy(list_borne);
     timer_stop();
     printf("Time to destroy the bornes data: ");
     timer_print();
-    
-
 
     timer_reset();
 
-
     timer_start();
     char *file_path2 = "data/Vehicules.csv";
-    //import the data in car.csv
+    // import the data i,n car.csv
     csv_import *file2 = csv_import_create(MAX_LINES_CARS, 3);
     csvParser(file_path2, 0, file2);
     timer_stop();
     printf("Time to parse the car file : ");
     timer_print();
     car_list *list_car = car_list_create();
-
     // import the data to a car_list
-    car_data_importer(file2, list_car);
+    car_data_importer(file, list_car);
     timer_stop();
     printf("Time to import the car data: ");
     timer_print();
-    csv_import_destroy(file2);
-
+    csv_import_destroy(file);
     // car_list_print(list_car);
     car_list_destroy(list_car);
     timer_stop();
