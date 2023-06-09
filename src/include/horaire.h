936bd794d6f0727738f70351b3cd6e4317791330
Maelan Tiger <Maelan.Tiger@telecomnancy.eu>
2023-06-09 18:18:01 +0200

    Added horaire & horaire_list struct and functions [+]   src/include/horaire.h [+]   src/include/horaire_list.h

---
+++ src/include/horaire.h
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef __HORAIRE_H__
+#define __HORAIRE_H__
+
+struct horaire {
+  int heure_depart;
+  int heure_arrivee;
+};
+
+typedef struct horaire horaire;
+
+horaire *horaire_create();
+
+void horaire_destroy(horaire *h);
+
+int horaire_getDepart(horaire *h);
+
+int horaire_getArrivee(horaire *h);
+
+void horaire_setDepart(horaire *h, int heure_depart);
+
+void horaire_setArrivee(horaire *h, int heure_arrivee);
+
+void horaire_print(horaire *h);
+
+#endif
---
+++ src/include/horaire_list.h
@@ -0,0 +1,48 @@
+#include "../include/horaire.h"
+
+
+#ifndef __HORAIRE_LIST_H__
+#define __HORAIRE_LIST_H__
+
+struct horaire_list {
+    int index;
+    struct horaire_list *next;
+    struct horaire_list *prev;
+    horaire *horaire;
+};
+
+typedef struct horaire_list horaire_list;
+
+// Creates a list of horaires
+horaire_list *horaire_list_create(void);
+
+// Destroys a list of horaires
+void horaire_list_destroy(horaire_list *list);
+
+// Get the index of a horaire in a list
+int horaire_list_getIndex(horaire_list *list);
+
+// Returns the horaire of the link
+horaire *horaire_list_gethoraire(horaire_list *list);
+
+// Get the next horaire in a list
+horaire_list *horaire_list_getNext(horaire_list *list);
+
+// Get the previous horaire in a list
+horaire_list *horaire_list_getPrev(horaire_list *list);
+
+// Appends a horaire to a list of horaires
+void horaire_list_append(horaire_list *list, horaire *horaire);
+
+// Insert a horaire in a list of horaires
+void horaire_list_insert(horaire_list *list, horaire *horaire);
+
+// Get the length of a list of horaires
+int horaire_list_length(horaire_list *list);
+
+
+// Prints a list of horaires
+void horaire_list_print(horaire_list *list);
+
+
+#endif
