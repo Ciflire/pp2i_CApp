594847392b9a23366beced8bf6f817ac43c75154
Maelan Tiger <Maelan.Tiger@telecomnancy.eu>
2023-06-09 17:58:56 +0200

    Updated python file to add grid (WIP)

--- utils/borne_cleaner.py
+++ utils/borne_cleaner.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def main():
-  # Reads data, deletes duplicates and saves it
-  bornes = pd.read_csv('data/borne_firststep.csv', sep=',').drop_duplicates()
-  
-  # Arrondi des coordonnées pour regrouper les bornes très proches (75m)
-  bornes = bornes.round({'consolidated_longitude': 3, 'consolidated_latitude': 3})
-  
-  # Regroupement des bornes par coordonnées en sommant le nombre de bornes et en prenant la puissance maximale
-  bornes = bornes.groupby(['consolidated_longitude', 'consolidated_latitude']).agg({'nbre_pdc': 'sum', 'puissance_nominale': 'max'}).reset_index()
-  # Export to csv
-  bornes.to_csv('data/borne_cleaned.csv', sep=',', index=False)
+    # Reads data, deletes duplicates and saves it
+    bornes = pd.read_csv('data/borne_firststep.csv', sep=',').drop_duplicates()
 
-  # Plot
-  plt.figure(0, figsize=[50, 50])
-  plt.scatter(bornes['consolidated_longitude'], bornes['consolidated_latitude'],
-            s=bornes['nbre_pdc'].values*10, c=bornes['puissance_nominale'].values, cmap='plasma')
-  plt.savefig('data/borne_cleaned.png')
-  
+    # Arrondi des coordonnées pour regrouper les bornes très proches (75m)
+    bornes = bornes.round(
+        {'consolidated_longitude': 3, 'consolidated_latitude': 3})
+
+    # Regroupement des bornes par coordonnées en sommant le nombre de bornes et en prenant la puissance maximale
+    bornes = bornes.groupby(['consolidated_longitude', 'consolidated_latitude']).agg(
+        {'nbre_pdc': 'sum', 'puissance_nominale': 'max'}).reset_index()
+    # Export to csv
+    bornes.to_csv('data/borne_cleaned.csv', sep=',', index=False)
+
+    # Plot
+    fig = plt.figure(0, figsize=[50, 50])
+    ax = fig.add_subplot(111)
+
+    ax.grid(which='both', color='lightgray', linestyle='--', linewidth=2)
+
+    plt.scatter(bornes['consolidated_longitude'], bornes['consolidated_latitude'],
+                s=bornes['nbre_pdc'].values*10, c=bornes['puissance_nominale'].values, cmap='plasma')
+    plt.savefig('data/borne_cleaned.png')
+
+
 if __name__ == '__main__':
-  main()
+    main()
