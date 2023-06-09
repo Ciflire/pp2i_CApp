b47f7e9c7432025f84417588cd4c177097197c19
Maelan Tiger <Maelan.Tiger@telecomnancy.eu>
2023-06-09 18:19:22 +0200

    Implemented horaire and horaire_list [+]   src/structs/horaire.c [+]   src/structs/horaire_list.c

---
+++ src/structs/horaire.c
@@ -0,0 +1,26 @@
+#include "../include/horaire.h"
+
+horaire *horaire_create() {
+  horaire *h = (horaire *)malloc(sizeof(horaire));
+  h->heure_depart = 0;
+  h->heure_arrivee = 0;
+  return h;
+}
+
+void horaire_destroy(horaire *h) { free(h); }
+
+int horaire_getDepart(horaire *h) { return h->heure_depart; }
+
+int horaire_getArrivee(horaire *h) { return h->heure_arrivee; }
+
+void horaire_setDepart(horaire *h, int heure_depart) {
+  h->heure_depart = heure_depart;
+}
+
+void horaire_setArrivee(horaire *h, int heure_arrivee) {
+  h->heure_arrivee = heure_arrivee;
+}
+
+void horaire_print(horaire *h) {
+  printf("Horaire: %d - %d\n", h->heure_depart, h->heure_arrivee);
+}
---
+++ src/structs/horaire_list.c
@@ -0,0 +1,80 @@
+#include "../include/horaire_list.h"
+
+// Creates a new list of horaires
+horaire_list *horaire_list_create() {
+  horaire_list *list = (horaire_list *)malloc(sizeof(horaire_list));
+  list->horaire = NULL;
+  list->next = list;
+  list->prev = list;
+  return list;
+}
+
+// Destroys a list of horaires
+void horaire_list_destroy(horaire_list *list) {
+  horaire_list *current = list;
+  horaire_list *next = NULL;
+  while (current != NULL) {
+    next = current->next;
+    horaire_destroy(current->horaire);
+    free(current);
+    current = next;
+  }
+}
+
+// Get the index of a horaire in a list
+int horaire_list_getIndex(horaire_list *list) { return list->index; }
+
+// Returns the horaire of the link
+horaire *horaire_list_gethoraire(horaire_list *list) { return list->horaire; }
+
+// Get the next horaire in a list
+horaire_list *horaire_list_getNext(horaire_list *list) { return list->next; }
+
+// Get the previous horaire in a list
+horaire_list *horaire_list_getPrev(horaire_list *list) { return list->prev; }
+
+// Appends a horaire to a list of horaires
+void horaire_list_append(horaire_list *list, horaire *horaire) {
+  horaire_list *new_horaire = horaire_list_create();
+  new_horaire->horaire = horaire;
+  new_horaire->index = horaire_list_length(list) + 1;
+  new_horaire->prev = list->prev;
+  new_horaire->next = list;
+  list->prev->next = new_horaire;
+  list->prev = new_horaire;
+}
+
+// Inserts a horaire in a list of horaires
+void horaire_list_insert(horaire_list *list, horaire *horaire) {
+  horaire_list *current = list;
+  bool beenInserted = false;
+  for (int i = 0; i < horaire_list_length(list); i++) {
+    if (current->horaire->heure_depart < horaire->heure_arrivee &&
+        !(beenInserted)) {
+      horaire_list_append(current, horaire);
+      beenInserted = true;
+    }
+    if (beenInserted) {
+      current->index = current->index + 1;
+    }
+    current = current->next;
+  }
+}
+
+// Get the length of a list of horaires
+int horaire_list_length(horaire_list *list) { return list->prev->index; }
+
+// Prints a list of horaires
+void horaire_list_print(horaire_list *list) {
+  if (horaire_list_length(list) == 0) {
+    printf("Liste vide\n");
+    return;
+  }
+  horaire_list *current = list->next;
+  while (current != list) {
+    printf("Horaire %d: %d - %d\n", current->index,
+           horaire_getDepart(current->horaire),
+           horaire_getArrivee(current->horaire));
+    current = current->next;
+  }
+}
