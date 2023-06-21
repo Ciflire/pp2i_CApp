5b3cc884673bf5a0863cff15d603504ad7e7a347
Killian <killian.thuillier@telecomnancy.eu>
2023-06-21 22:22:49 +0200

    importer bug fix 2

--- src/utils/data_importer.c
+++ src/utils/data_importer.c
@@ -37,18 +37,18 @@ void car_list_dataImporter(line_array *file, car_list *list_car) {
 void borne_list_memoryImporter(line_array *pdcList, borne_list *list_borne) {
   printf("In importer\n");
   borne_list *bl = list_borne;
+  char *arr = calloc(256,sizeof(char));
+  char *dep = calloc(256,sizeof(char));
   for (int i = 0; i < line_array_getSize(pdcList); i++) {
     line *current_line = pdcList->line[i];
-    for (int j = 0; j < bl->borne->pdc-1; j++) {
-      char *pdc = current_line->info[j];
+    for (int j = 0; j < bl->borne->pdc - 1; j++) {
+      char *pdc = current_line->info[j];/* 
       printf("pdc size : %lu\n", strlen(pdc));
-      printf("pdc : %s\n", pdc);
+      printf("pdc : %s\n", pdc); */
       int k = 0;
       int l = 0;
       bool in_parenthesis = false;
       bool before_comma = true;
-      char *arr = malloc(sizeof(char) * (4*strlen(pdc)+1));
-      char *dep = malloc(sizeof(char) * (strlen(pdc)+1));
 
       while (pdc[k] != '\0') {
         if (pdc[k] == '[') {
@@ -59,16 +59,20 @@ void borne_list_memoryImporter(line_array *pdcList, borne_list *list_borne) {
         } else if (pdc[k] == ')') {
           in_parenthesis = false;
           dep[l] = '\0';
+          printf("arr : %s\n", arr);
+          printf("dep : %s\n", dep);
           horaire_list_insert(bl->borne->horairePdc[j],
                               horaire_createWithValues(atoi(arr), atoi(dep)));
-          arr = malloc(sizeof(char) * strlen(pdc));
-          dep = malloc(sizeof(char) * strlen(pdc));
           l = 0;
         } else if (pdc[k] == ',' && in_parenthesis) {
           arr[l] = '\0';
           l = 0;
           before_comma = false;
-        } else if (pdc[k] == ']') {
+        } else if (pdc[k] == ',' && !in_parenthesis) {
+          l = 0;
+          before_comma = true;
+        }
+        else if (pdc[k] == ']') {
           k++;
         } else {
           if (before_comma) {
