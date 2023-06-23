b890e746553c2032eb5f78f28c9ff99a623272e3
Maelan Tiger <Maelan.Tiger@telecomnancy.eu>
2023-06-23 19:50:32 +0200

    Added heatmap creation for simulation [+]   utils/heatmap.py [~]   app.py [~]   static/style.css [~]   templates/simul.html [~]   templates/travel.html [+]   static/images/bornes.png [+]   static/images/heatmap.png

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from utils.heatmap import heatmap
 import requests
 import sys
 import os
@@ -6,12 +7,13 @@ from random import *
 app = Flask(__name__)
 
 
-#read the file path and return the string stored in the fist line
+# read the file path and return the string stored in the fist line
 def read_response_file(path):
     file = open(path, "r")
     response = file.read().strip()
     file.close()
     return response
+
 
 def read_data_file(path):
     file = open(path, "r")
@@ -20,13 +22,16 @@ def read_data_file(path):
     response = eval(list[-2])
     return response
 
+
 @app.route('/')
 def index():
     return redirect('/home')
 
+
 @app.route('/home')
 def home():
     return render_template('home.html')
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -39,18 +44,22 @@ def search():
         max_waiting_time = request.form['max_waiting_time']
         battery_minimum = request.form['battery_minimum']
         return redirect('/search/' + departure + '/' + arrival + '/' + car + '/' + heure_depart + '/' + max_charging_time + '/' + max_waiting_time + '/' + battery_minimum)
-    
+
+
 @app.route('/search/<departure>/<arrival>/<car>/<heure_depart>/<max_charging_time>/<max_waiting_time>/<battery_minimum>')
 def search_result(departure, arrival, car, heure_depart, max_charging_time, max_waiting_time, battery_minimum):
-    os.system("./bin/app " + car +" "+departure+" "+heure_depart+" "+arrival+" "+battery_minimum+" "+max_charging_time+" "+max_waiting_time)
+    os.system("./bin/app " + car + " "+departure+" "+heure_depart+" " +
+              arrival+" "+battery_minimum+" "+max_charging_time+" "+max_waiting_time)
     link = read_response_file("data/response_link.txt")
     list_borne = read_data_file("data/response_borne.txt")
     list_horaires = read_data_file("data/response_horaires.txt")
     return render_template('search.html', departure=departure, arrival=arrival, car=car, heure_depart=heure_depart, max_charging_time=max_charging_time, max_waiting_time=max_waiting_time, battery_minimum=battery_minimum, link=link, list_borne=list_borne, list_horaires=list_horaires)
 
+
 @app.route('/simulation', methods=['GET'])
 def simulation():
     return render_template('simul.html', nb_cars=-1)
+
 
 @app.route('/simul', methods=['POST'])
 def simul():
@@ -58,40 +67,42 @@ def simul():
         nb_cars = request.form['nb_cars']
         seed = randint(0, 1000000)
         return redirect('/simul/' + str(nb_cars) + '/' + str(seed))
-    
+
+
 @app.route('/simul/<nb_cars>/<seed>')
 def simul_result(nb_cars, seed):
-    os.system("./bin/app " + str(nb_cars) +" "+str(seed))
+    os.system("./bin/app " + str(nb_cars) + " "+str(seed))
+    heatmap()
     return render_template('simul.html', nb_cars=nb_cars)
-    
+
+
 @app.route('/reset', methods=['GET'])
 def reset():
-    #reset the link data file
-    f = open("data/response_link.txt","w")
+    # reset the link data file
+    f = open("data/response_link.txt", "w")
     f.write("")
     f.close()
-    #reset the horaires data file
-    f = open("data/response_horaires.txt","w")
+    # reset the horaires data file
+    f = open("data/response_horaires.txt", "w")
     f.write("")
     f.close()
-    #reset the borne data file
-    f = open("data/response_borne.txt","w")
+    # reset the borne data file
+    f = open("data/response_borne.txt", "w")
     f.write("")
     f.close()
-    #reset the pdc data file
-    f = open("data/response_pdc.txt","w")
-    reset=""
-    for i in range (64501) :
-        reset+="[]\n"
+    # reset the pdc data file
+    f = open("data/response_pdc.txt", "w")
+    reset = ""
+    for i in range(64501):
+        reset += "[]\n"
     print(reset)
     f.write(reset)
     f.close()
-    #reset the max pdc counter
-    f = open("data/nb_max_pdc.txt","w")
+    # reset the max pdc counter
+    f = open("data/nb_max_pdc.txt", "w")
     f.write("0\n")
     f.close()
     return redirect('/home')
-
 
 
 if __name__ == '__main__':
---
+++ utils/heatmap.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def heatmap():
+    a = np.loadtxt("data/borne_cleaned.csv", skiprows=1, delimiter=',')
+
+    with open('data/response_pdc.txt', 'r') as f:
+        data = f.readlines()
+
+    usage = np.zeros((len(a), 300))
+
+    i = 0
+    j = 0
+    for pdc in a[:, 2]:
+        i += 1
+        for _ in range(int(pdc)):
+            liste = eval(data[j][:-1])
+            chemin = np.array(liste)
+            j += 1
+            for ah in chemin:
+                usage[i][ah[0] // 10: ah[1] // 10] += 1
+
+    fig = plt.figure(figsize=(40, 40))
+    plt.scatter(a[:, 0], a[:, 1], c='#d44', s=1, figure=fig)
+    plt.scatter(a[:, 0], a[:, 1], cmap=plt.cm.summer, c=usage.sum(
+        axis=1)/a[:, 2], s=usage.sum(axis=1)*100/a[:, 2], figure=fig)
+
+    fig.savefig('static/images/bornes.png')
+
+    fig1 = plt.figure(figsize=(40, 40))
+    plt.imshow(usage, interpolation='nearest',
+               cmap=plt.cm.Greens, aspect='auto')
+    fig1.savefig('static/images/heatmap.png')
+
+    np.savetxt('data/usage.csv', usage, delimiter=',', fmt='%d')
+
+
+heatmap()
